f00a6b7dde1bbabda5c54382741a37de9df17cc6
David Bayless <David.C.Bayless15@gmail.com>
2017-01-15 21:35:37 -0700

    add basic syntax

--- query.js
+++ query.js
@@ -1,3 +1,36 @@
-var query() {
-  
+function query() {
+  return new Query();
+}
+
+function Query() {
+  this.hasFrom = false;
+  this.selected = false;
+
+  this.source = [];
+
+  this.groupBy = defaultGroupBy;
+}
+
+Query.prototype.select = function(groupBy) {
+  this.selected = true;
+  this.groupBy = groupBy || defaultGroupBy;
+  return this;
+}
+
+Query.prototype.from = function(source) {
+  this.hasFrom = true;
+  this.source = source;
+  return this;
+}
+
+Query.prototype.execute = function() {
+  return this.source.map(this.groupBy);
+}
+
+Query.prototype.where = function() {
+  return this;
+}
+
+function defaultGroupBy(element) {
+  return element;
 }
